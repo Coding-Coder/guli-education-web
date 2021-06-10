6f5e641b7f129092f6d8be613a31831d9a9fd448
lxy <aethon47@163.com>
2021-06-10 16:31:08 +0800

    feat:完成用户的注册登录以及登录后显示用户信息

--- template/layouts/default.vue
+++ template/layouts/default.vue
@@ -29,42 +29,30 @@
           </ul>
           <!-- / nav -->
           <ul class="h-r-login">
-            <li id="no-login">
-              <a href="/sing_in" title="登录">
-                <em class="icon18 login-icon">&nbsp;</em>
-                <span class="vam ml5">登录</span>
-              </a>
-              |
-              <a href="/sign_up" title="注册">
-                <span class="vam ml5">注册</span>
-              </a>
-            </li>
-            <li class="mr10 undis" id="is-login-one">
-              <a href="#" title="消息" id="headerMsgCountId">
-                <em class="icon18 news-icon">&nbsp;</em>
-              </a>
-              <q class="red-point" style="display: none">&nbsp;</q>
-            </li>
-            <li class="h-r-user undis" id="is-login-two">
-              <a href="#" title>
-                <img
-                  src="~/assets/img/avatar-boy.gif"
-                  width="30"
-                  height="30"
-                  class="vam picImg"
-                  alt
-                />
-                <span class="vam disIb" id="userName"></span>
-              </a>
-              <a
-                href="javascript:void(0)"
-                title="退出"
-                onclick="exit();"
-                class="ml5"
-                >退出</a
-              >
-            </li>
             <!-- /未登录显示第1 li；登录后显示第2，3 li -->
+            <li v-if="!loginInfo.id" id="no-login">
+                <a href="/login" title="登录">
+                    <em class="icon18 login-icon">&nbsp;</em>
+                    <span class="vam ml5">登录</span>
+                </a>
+                |
+                <a href="/register" title="注册">
+                    <span class="vam ml5">注册</span>
+                </a>
+            </li>
+            <li v-if="loginInfo.id" id="is-login-one" class="mr10">
+                <a id="headerMsgCountId" href="#" title="消息">
+                    <em class="icon18 news-icon">&nbsp;</em>
+                </a>
+                <q class="red-point" style="display: none">&nbsp;</q>
+            </li>
+            <li v-if="loginInfo.id" id="is-login-two" class="h-r-user">
+                <a href="/ucenter" title>
+                    <img :src="loginInfo.avatar" width="30" height="30" class="vam picImg" alt>
+                    <span id="userName" class="vam disIb">{{ loginInfo.nickname }}</span>
+                </a>
+                <a href="javascript:void(0);" title="退出" @click="logout()" class="ml5">退出</a>
+            </li>
           </ul>
           <aside class="h-r-search">
             <form action="#" method="post">
@@ -147,5 +135,35 @@ import "~/assets/css/theme.css";
 import "~/assets/css/global.css";
 import "~/assets/css/web.css";
 
-export default {};
+import cookie from 'js-cookie'
+
+export default {
+  data() {
+    return {
+        token:'',
+        loginInfo: {
+          id: '',
+          age: '',
+          avatar: '',
+          mobile: '',
+          nickname: '',
+          sex: ''
+        }
+    }
+  },
+  created() {
+    this.showInfo()
+  },
+  methods:{
+    //创建方法，从cookie获取用户信息
+    showInfo() {
+      //从cookie获取用户信息
+      var userStr = cookie.get('guli_ucenter')
+      // 把字符串转换json对象(js对象)
+      if(userStr) {
+        this.loginInfo = JSON.parse(userStr)
+      }
+    }
+  }
+};
 </script>
--- template/utils/request.js
+++ template/utils/request.js
@@ -1,7 +1,26 @@
 import axios from 'axios'
+import { MessageBox, Message } from 'element-ui'
+import cookie from 'js-cookie'
 // 创建axios实例
 const service = axios.create({
   baseURL: 'http://localhost:9001', // api的base_url
   timeout: 20000 // 请求超时时间
 })
+
+//创建拦截器  http request 拦截器
+service.interceptors.request.use(
+  config => {
+    //debugger
+    //判断cookie里面是否有名称是guli_token数据
+    if (cookie.get('guli_token')) {
+      //把获取cookie值放到header里面
+      config.headers['token'] = cookie.get('guli_token');
+    }
+    return config
+  },
+  err => {
+    return Promise.reject(err);
+  }
+)
+
 export default service
