8bd568a8a6586e932eba68b10648f882f74d1b1b
lxy <aethon47@163.com>
2021-06-17 13:54:39 +0800

    feat:完成统计分析功能

--- vue-admin-template-master/src/router/index.js
+++ vue-admin-template-master/src/router/index.js
@@ -132,6 +132,28 @@ export const constantRouterMap = [
   },
 
   {
+    path: '/sta',
+    component: Layout,
+    redirect: '/sta/create',
+    name: '统计分析',
+    meta: { title: '统计分析', icon: 'example' },
+    children: [
+      {
+        path: 'create',
+        name: '生成数据',
+        component: () => import('@/views/sta/create'),
+        meta: { title: '生成数据', icon: 'table' }
+      },
+      {
+        path: 'show',
+        name: '图表显示',
+        component: () => import('@/views/sta/show'),
+        meta: { title: '图表显示', icon: 'tree' }
+      }
+    ]
+  },
+
+  {
     path: '/example',
     component: Layout,
     redirect: '/example/table',
